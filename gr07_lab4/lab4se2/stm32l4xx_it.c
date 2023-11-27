07478c93b3c4eab2efe227e7315d09f23e456def
Lollo2001 <91013499+Lollo2001@users.noreply.github.com>
2023-11-27 11:36:48 +0100

    Update stm32l4xx_it.c

--- gr07_lab4/lab4se2/stm32l4xx_it.c
+++ gr07_lab4/lab4se2/stm32l4xx_it.c
@@ -181,9 +181,10 @@ void TIM4_IRQHandler(void)
 {
   /* USER CODE BEGIN TIM4_IRQn 0 */
 	if(LL_TIM_IsActiveFlag_CC1(TIM4)){
-					LL_TIM_ClearFlag_CC1(TIM4);
-					LL_TIM_WriteReg(TIM4, CCR1, LL_TIM_ReadReg(TIM4, CCR1) + 1);
-				}
+		LL_TIM_ClearFlag_CC1(TIM4);
+		LL_TIM_WriteReg(TIM4, CCR1, LL_TIM_ReadReg(TIM4, CCR1) + 1);
+		LL_GPIO_WriteReg(GPIOE, ODR, LL_GPIO_ReadReg(GPIOE, ODR) ^ (1 << 0)); // toogle porta IO0	
+	}
   /* USER CODE END TIM4_IRQn 0 */
   /* USER CODE BEGIN TIM4_IRQn 1 */
 
